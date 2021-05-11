be469c7db0cc6ca61800022453debed9edd46c4b
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2021-05-11 08:26:44 +0200

    testing act

--- test/labb3.test.js
+++ test/labb3.test.js
@@ -1,105 +1,97 @@
-import TestRenderer from 'react-test-renderer';
-import { MockedProvider } from '@apollo/client/testing';
+import TestRenderer from "react-test-renderer";
+import { MockedProvider } from "@apollo/client/testing";
 import CoursesList, { ALL_COURSES_QUERY } from "../components/coursesList";
-import Course, {CURRENT_COURSE_QUERY} from "../pages/courses/[course]"
-import Home from '../pages/index'
-import Head from 'next/head'
-import Layout from '../components/sharedLayout'
+import Course, { CURRENT_COURSE_QUERY } from "../pages/courses/[course]";
+import Home from "../pages/index";
+import Head from "next/head";
+import Layout from "../components/sharedLayout";
 import StudentList, { ALL_STUDENTS_QUERY } from "../components/studentList";
 
-
-it('gets student firstname', async () => {
+it("gets student firstname", async () => {
   const studentmock = {
-        request: {
-          query: ALL_STUDENTS_QUERY
-        },
-        error: new Error(),
-    };
+    request: {
+      query: ALL_STUDENTS_QUERY,
+    },
+    error: new Error(),
+  };
   const component = TestRenderer.create(
     <MockedProvider mocks={[studentmock]} addTypename={false}>
       <StudentList />
-    </MockedProvider>,
+    </MockedProvider>
   );
 
-  await new Promise(resolve => setTimeout(resolve, 0));
+  await TestRenderer.act(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+  });
 
   const tree = component.toJSON();
-  expect(tree.children).toContain('Error loading students.');
-
+  expect(tree.children).toContain("Error loading students.");
 });
 
-it('loding students list', () => {
+it("loding students list", () => {
   const studentmock = [
     {
-        request: {
-          query: ALL_STUDENTS_QUERY
-        },
-        result: {      
-          data: {
-            queryStudent: [
-              {
-                firstName: "Bonny",
-                lastName: "Winter"
-              }
-            ],
-          }
+      request: {
+        query: ALL_STUDENTS_QUERY,
       },
-    }
-];
+      result: {
+        data: {
+          queryStudent: [
+            {
+              firstName: "Bonny",
+              lastName: "Winter",
+            },
+          ],
+        },
+      },
+    },
+  ];
   const component = TestRenderer.create(
     <MockedProvider mocks={studentmock} addTypename={false}>
       <StudentList />
-    </MockedProvider>,
+    </MockedProvider>
   );
 
   const tree = component.toJSON();
-  expect(tree.children).toContain('Loading students');
-
+  expect(tree.children).toContain("Loading students");
 });
 
-
-
-
-
-it('loads the courses', () => {
+it("loads the courses", () => {
   const mocks = [
     {
-        request: {
-          query: ALL_COURSES_QUERY
-        },
-        result: {      
-          data: {
-            queryCourse: [
-              {
-                name: "React"
-              },
-              {
-                name: "JavaScript"
-              },
-              {
-                name: "Java"
-              }
-            ]
-          },
+      request: {
+        query: ALL_COURSES_QUERY,
       },
-    }
-];
+      result: {
+        data: {
+          queryCourse: [
+            {
+              name: "React",
+            },
+            {
+              name: "JavaScript",
+            },
+            {
+              name: "Java",
+            },
+          ],
+        },
+      },
+    },
+  ];
   const component = TestRenderer.create(
     <MockedProvider mocks={mocks} addTypename={false}>
       <CoursesList />
-    </MockedProvider>,
+    </MockedProvider>
   );
 
   const tree = component.toJSON();
-  expect(tree.children).toContain('Loading courses');
+  expect(tree.children).toContain("Loading courses");
 });
 
+it("Should render home components", () => {
+  const home = TestRenderer.create(<Home />);
 
-it('Should render home components', () => {
-  const home = TestRenderer.create(<Home/>);
-  
   expect(home.root.findAllByType(Head));
   expect(home.root.findAllByType(Layout));
-})
-
-
+});
